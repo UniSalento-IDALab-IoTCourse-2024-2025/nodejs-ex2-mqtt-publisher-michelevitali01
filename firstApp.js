2152a9cc025ceea80e34100fde367ddbf9b28d0b
mvitali01 <105487401+mvitali01@users.noreply.github.com>
2025-04-11 13:15:44 +0200

    eliminazione http

--- firstApp.js
+++ firstApp.js
@@ -1,32 +1,32 @@
-const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
+//const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
 const http = require('node:http');
 
 // Setup sensor, exit if failed
-var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
-var sensorPin = 4; // The GPIO pin number for sensor signal
-if (!sensorLib.initialize(sensorType, sensorPin))
-{
+//var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
+//var sensorPin = 4; // The GPIO pin number for sensor signal
+//if (!sensorLib.initialize(sensorType, sensorPin))
+//{
 //print a warning message in the console
-console.warn('Failed to initialize sensor');
-process.exit(1);
-}
+//console.warn('Failed to initialize sensor');
+//process.exit(1);
+//}
 
 // Automatically update sensor value every 2 seconds
 //we use a nested function (function inside another function)
 setInterval(function() {
-	var readout = sensorLib.read();
+	//var readout = sensorLib.read();
 
-	console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-	console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
-	var temperature = readout.temperature.toFixed(1);
+	//console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
+	//console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
+	//var temperature = readout.temperature.toFixed(1);
 
 	const postData = JSON.stringify({
 		'sensor': 'ID1',
 		'timestamp': 12345678,
-		'temperature': temperature
+		'temperature': Math.random()
 	})
 	const options = {
-	hostname: '192.168.137.1',
+	hostname: 'localhost',
 	port: 3000,
 	path: '/temperature',
 	method: 'POST',
